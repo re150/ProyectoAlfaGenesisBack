b7760a4cd7422beae4788860580975206fcdf283
re150 <a21110175@ceti.mx>
2024-07-17 17:21:58 -0600

    configuracion del gitignore

--- src/main/java/com/alfagenesi/com/BackAG/BackAgApplication.java
+++ src/main/java/com/alfagenesi/com/BackAG/BackAgApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackAgApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackAgApplication.class, args);
 	}
-
 }
